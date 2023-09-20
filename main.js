c1799f287e21486234e6a8b193ebf4aee733191b
DavidAlbUwU <117684736+DavidAlbUwU@users.noreply.github.com>
2023-09-20 16:06:10 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 song = "";
+song2 = "";
+song_status = "";
+song2_status = "";
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
 
 rightWristX = 0;
 rightWristY = 0;
@@ -6,7 +11,8 @@ rightWristY = 0;
 leftWristX = 0;
 leftWristY = 0;
 function preload(){
-    song = loadSound("Is This Really Happening _-TrackTribe.mp3", "Drop the tapes - TrackTribe.mp3");
+    song = loadSound("musica1.mp3");
+    song2 = loadSound("musica2.mp3");
 }
 
 function setup(){
@@ -15,11 +21,35 @@ function setup(){
     video = createCapture(VIDEO);
     video.hide();
     poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on("pose", gotPoses)
+    poseNet.on("pose", gotPoses);
+}
+
+function modelLoaded(){
+    console.log("PoseNet inicializado!!!(confetes)");
 }
 
 function draw(){
     image(video ,0, 0, 600, 500);
+    fill("#7FB3D5"); //"#EC7063"
+    stroke("#7FB3D5"); //"#EC7063"
+    if (scoreRightWrist > 0.2){
+        circle(rightWristX, rightWristY, 20);
+        song2.stop();
+        if(song_status == false){
+            song.play();
+            document.getElementById("song").innerHTML = "Tocando Drop the Tapes";
+        }
+    }
+    if (scoreLeftWrist > 0.2){
+        
+    
+        circle(leftWristX, leftWristY, 20);
+        song.stop();
+        if(song2_status == false){
+        song2.play();
+        document.getElementById("song").innerHTML = "Tocando Is This Really Heappening";
+        }
+    }
 }
 
 function play(){
@@ -31,12 +61,15 @@ function play(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
         console.log("rightWristX: " + rightWristX + "rightWristY :" + rightWristY)
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
-        rightWristY = results[0].pose.rightWrist.y;
         console.log("leftWristX: " + leftWristX + "leftWristY :" + leftWristY)
     }
 }
